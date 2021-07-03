7ad43bcbe7f109e1fd3e438fc704e265c9ee0706
amanharitsh123 <amanharitsh123@gmail.com>
2021-07-03 17:26:00 +0000

    Download service done

--- download/main.py
+++ download/main.py
@@ -9,20 +9,9 @@ UPLOAD_FOLDER = '../upload/upload'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/download', methods=['POST'])
-def downlaod():
-    result = request.form.to_dict(flat=False)
-    data = {'userid':result['userid'][0], 'image':result['image'][0]}
-    userid = result['userid'][0]
-    image = result['image'][0]
-    path = os.path.join(app.config['UPLOAD_FOLDER'])
-    print(path)
-    return send_from_directory(path, image)
-
 @app.route('/', methods=['GET', 'POST'])
 def downlaod_file():
-    image = request.args.get('image', default = 1, type = int)
-    if image == 1:
+    if request.method=='GET':
         data = get_all_filenames()
         loader = jinja2.FileSystemLoader('templates/download.html')
         env = jinja2.Environment(loader=loader)
@@ -30,7 +19,14 @@ def downlaod_file():
         return template.render(items=data)
     else:
         # Check for connections and file download
-        pass
+        result = request.form.to_dict(flat=False)
+        data = {'userid':result['userid'][0], 'image':result['image'][0]}
+        userid = result['userid'][0]
+        image = result['image'][0]
+        path = os.path.join(app.config['UPLOAD_FOLDER'])
+        print(path)
+        return send_from_directory(path, image)
+        
 
 
 
